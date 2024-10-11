2a7082f3f9b2839438f86a87db99ae524baf0e24
Andres Felipe Garces Aponte <slab8191@gmail.com>
2024-10-10 22:24:22 -0500

    Commit de correccion

--- scripts.js
+++
@@ -1,7 +0,0 @@
-  const menuToggle = document.querySelector('.menu-toggle');
-  const headerOptions = document.querySelector('.header-options');
-
-  menuToggle.addEventListener('click', () => {
-    headerOptions.classList.toggle('active');
-  });
-
